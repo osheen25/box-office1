20513213d1df833197539b1d7ea84d85108c8b8c
osheen <osheensingh222@gmail.com>
2021-06-02 13:26:45 +0530

    Updates

---
+++ precache-manifest.1fb8e3c8662f4e8747faef1697aaf152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f0993ebb821077aca3834095caee9e",
+    "url": "/box-office1/index.html"
+  },
+  {
+    "revision": "463a3b5a03729968f43e",
+    "url": "/box-office1/static/css/main.3cd6ee85.chunk.css"
+  },
+  {
+    "revision": "e097ce2523112642d82b",
+    "url": "/box-office1/static/js/2.1da83c24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office1/static/js/2.1da83c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463a3b5a03729968f43e",
+    "url": "/box-office1/static/js/main.ac8aa2f7.chunk.js"
+  },
+  {
+    "revision": "c4922a5f00893a7c3e11",
+    "url": "/box-office1/static/js/runtime-main.93c18862.js"
+  }
+]);
